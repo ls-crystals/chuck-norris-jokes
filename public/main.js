78427239bbb3baa8013c260455dbd21003e88961
crystal <crystalrobertson236@gmail.com>
2020-01-11 09:56:15 -0500

    final commit

---
+++ public/main.js
@@ -0,0 +1,10 @@
+let x = Math.floor(Math.random() * Math.floor(100));
+  return x
+
+if (x > 50 )
+getElementById('btn1')
+
+else if (x < 50 ) {
+  getElementById('btn2')
+
+}
--- server.rb
+++ server.rb
@@ -12,4 +12,19 @@ get '/' do
 
 end
 
-get ''
+get '/joke/:id'do
+  joke_id = params['id'].to_i
+
+  @found_joke = ChuckNorris::JokeFinder.find_joke(joke_id)
+end
+get '/'do
+  puts params
+  puts params['name']
+  @chuck = ChuckNorris.new
+  @chuck.jokes
+  @chuck.jokes@chuck.personal(params['name'], '')
+end
+
+post '/:name' do
+  puts 'Hello' + params['name']
+end
